da00af3b7227e1e918fa3edfb606224a9585b7d2
Eleftherios Laskaridis <eleftherios.laskaridis@bestsecret.com>
2026-03-30 14:07:09 +0300

    Uses CapitalCase for controller classes

--- backend/static/js/controllers/index.js
+++ backend/static/js/controllers/index.js
@@ -1,19 +1,19 @@
-import clipCardController from "./clip-card-controller.js"
-import filterDrawerController from "./filter-drawer-controller.js"
-import filterSidebarController from "./filter-sidebar-controller.js"
-import filterTriggerRowController from "./filter-trigger-row-controller.js"
-import globalAuthQuickSearchController from "./global-auth-quick-search-controller.js"
-import globalThemeToggleController from "./global-theme-toggle-controller.js"
-import labelFilterOptionsController from "./label-filter-options-controller.js"
+import ClipCardController from "./clip-card-controller.js"
+import FilterDrawerController from "./filter-drawer-controller.js"
+import FilterSidebarController from "./filter-sidebar-controller.js"
+import FilterTriggerRowController from "./filter-trigger-row-controller.js"
+import GlobalAuthQuickSearchController from "./global-auth-quick-search-controller.js"
+import GlobalThemeToggleController from "./global-theme-toggle-controller.js"
+import LabelFilterOptionsController from "./label-filter-options-controller.js"
 
 const CONTROLLERS = [
-  ["clip-card", clipCardController],
-  ["filter-drawer", filterDrawerController],
-  ["filter-sidebar", filterSidebarController],
-  ["filter-trigger-row", filterTriggerRowController],
-  ["global-auth-quick-search", globalAuthQuickSearchController],
-  ["global-theme-toggle", globalThemeToggleController],
-  ["label-filter-options", labelFilterOptionsController],
+  ["clip-card", ClipCardController],
+  ["filter-drawer", FilterDrawerController],
+  ["filter-sidebar", FilterSidebarController],
+  ["filter-trigger-row", FilterTriggerRowController],
+  ["global-auth-quick-search", GlobalAuthQuickSearchController],
+  ["global-theme-toggle", GlobalThemeToggleController],
+  ["label-filter-options", LabelFilterOptionsController],
 ]
 
 export function registerControllers(application) {
